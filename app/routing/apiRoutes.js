2aa8437ca8bf368635428ad510c5dcd8aa95b402
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-10-06 16:54:51 -0500

    added css

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,49 +1,46 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
 var friends = require("../data/friends");
-// var waitListData = require("../data/waitinglistData");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    
-    // friends.push(req.body);
-    //   res.json(true);
+
     console.log(req.body);
     res.status(200).end();
+
+    var newUserScores = [];
+
+    var addNewUser = function () {
+        newUserScores.push(req.body.question1);
+        newUserScores.push(req.body.question2);
+        newUserScores.push(req.body.question3);
+        newUserScores.push(req.body.question4);
+        newUserScores.push(req.body.question5);
+        newUserScores.push(req.body.question6);
+        newUserScores.push(req.body.question7);
+        newUserScores.push(req.body.question8);
+        newUserScores.push(req.body.question9);
+        newUserScores.push(req.body.question10);
+    };
+
+    addNewUser();
+    console.log(newUserScores);
+
+    // for (var i = 0; i < friends.length; i++) {
+    //     // var difference = 0;
+    //     // difference.push(i.scores)
+    //     i.scores.forEach(element => {
+    //         console.log(element);
+    //       });
+    //     // for (var j = 0; j < newUserScores.length; i++) {
+
+    //     // }
+    // };
     
     // if (friends.length < 5) {
     //   friends.push(req.body);
@@ -55,15 +52,10 @@ module.exports = function(app) {
     // }
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
 
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
     friends.length = [];
-    // waitListData.length = [];
-
     res.json({ ok: true });
   });
 };
--- server.js
+++ server.js
@@ -21,20 +21,10 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// =============================================================================
-// LISTENER
 // The below code effectively "starts" our server
-// =============================================================================
-
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
